6a911509b06f7af3e9c8be23720c7b93fdbe5576
uditanand06 <uditanand84@gmail.com>
2021-03-25 18:03:28 +0530

    ui

--- frontend/src/components/hy/Navy/navbar.js
+++ frontend/src/components/hy/Navy/navbar.js
@@ -25,7 +25,7 @@ export default function ButtonAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" style={{ background: '#0f0c29' }}>
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
