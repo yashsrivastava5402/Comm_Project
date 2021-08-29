9939e647cd12a9675175843e79f92471ff7c8966
SHAURYA369 <shaurya7singh@gmail.com>
2021-08-29 20:48:17 +0530

    Update validated.jsx

--- frontend/src/components/validated/validated.jsx
+++ frontend/src/components/validated/validated.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import Lang from '../hy/Language/Language'
-import Navy from '../hy/Navy/navbar'
+import React,{useEffect} from "react";
+import Lang from "../hy/Language/Language";
+import Navy from "../hy/Navy/navbar";
 
 
+const Validated = (props) => {
+    
+    useEffect(() => {
+        if(sessionStorage.getItem('User')===null || sessionStorage.getItem('User')==='')
+        props.history.replace("/login");
+      
+    }, [])
 
-const Validated = (props) => <>
-
-<Navy/>
-<Lang {...props}/>
-
-
-
-
-</>;
+    return (
+    <>
+      <Navy />
+      <Lang {...props} />
+    </>
+  );
+};
 
 export default Validated;
